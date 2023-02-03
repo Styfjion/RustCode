a0ab945d4553c62130fbfc49555bf73581c02522
Styfjion <33060090+Styfjion@users.noreply.github.com>
2023-02-03 11:51:08 +0800

    Update wrapper_cancel_token.rs

--- src/wrapper_cancel_token.rs
+++ src/wrapper_cancel_token.rs
@@ -1,25 +1,40 @@
-use std::borrow::Borrow;
-use std::future::{Future, poll_fn};
-use std::ops::Deref;
-use std::sync::Arc;
 use tokio_util::sync::CancellationToken;
-use common_exception::Result;
 
+#[tonic::async_trait]
+pub trait AsyncCallback {
+    async fn execute_call_back(&self);
+}
+
+struct DummyAction {
+
+}
+
+#[tonic::async_trait]
+impl AsyncCallback for DummyAction {
+    async fn execute_call_back(&self) {
+        println!("No action");
+    }
+}
 
 pub struct WrapperCancelToken {
     cancel_token: CancellationToken,
-    register_action: Arc<dyn Future<Output=Result<()>> + Send + 'static + Sync>
+    action: Box<dyn AsyncCallback + Send + Sync + 'static>,
 }
 
+impl Clone for WrapperCancelToken {
+    fn clone(&self) -> Self {
+        WrapperCancelToken {
+            cancel_token: self.cancel_token.clone(),
+            action: Box::new(DummyAction{}),
+        }
+    }
+}
 
 impl Default for WrapperCancelToken {
     fn default() -> Self {
         WrapperCancelToken {
             cancel_token: CancellationToken::new(),
-            register_action: Arc::new(async move {
-                println!("No action");
-                Ok(())
-            })
+            action: Box::new(DummyAction{}),
         }
     }
 }
@@ -27,13 +42,13 @@ impl Default for WrapperCancelToken {
 
 impl WrapperCancelToken {
 
-    pub fn register_action(&mut self, action: impl Future<Output=Result<()>> + Send + 'static + Sync) {
-        self.register_action = Arc::new(action);
+    pub fn register_action(&mut self, action: impl AsyncCallback + Send + Sync + 'static) {
+        self.action = Box::new(action);
     }
 
-    pub async fn cancel(&self) {
+    pub async fn cancel_with_post_action(&self) {
         self.cancel_token.cancel();
-        tokio::spawn(self.register_action.deref().await);
+        self.action.execute_call_back().await;
     }
 
     pub fn inner(&self) -> CancellationToken {
@@ -43,13 +58,53 @@ impl WrapperCancelToken {
 
 #[cfg(test)]
 mod test {
-    use std::ops::Deref;
-    use crate::utils::wrapper_cancel_token::WrapperCancelToken;
+    use std::collections::HashMap;
+    use std::sync::Arc;
+    use tokio::select;
+    use tokio::sync::Mutex;
+    use crate::utils::wrapper_cancel_token::{AsyncCallback, WrapperCancelToken};
 
     #[tokio::test]
-    async fn test_fun() {
+    async fn test_init_func() {
         let wrapper_token = WrapperCancelToken::default();
-        let _ = wrapper_token.register_action.deref()().await;
+        wrapper_token.cancel_with_post_action().await;
+    }
+
+    struct WrapperData {
+        data: Arc<Mutex<HashMap<i32, i32>>>
+    }
+
+    #[tonic::async_trait]
+    impl AsyncCallback for WrapperData {
+        async fn execute_call_back(&self) {
+            let mut data = self.data.lock().await;
+            println!("Task has been cancelled");
+            data.insert(2, 2);
+            println!("data is {:?}", data);
+        }
+    }
+
+    #[tokio::test]
+    async fn test_register() {
+        let mut wrapper_token = WrapperCancelToken::default();
+        let wrapper_data = WrapperData{data:Arc::new(Mutex::new(HashMap::from([(1, 1)])))};
+        wrapper_token.register_action(wrapper_data);
+        let clone_token = wrapper_token.inner().clone();
+
+        let join_handle = tokio::spawn(async move {
+            select! {
+                _ = clone_token.cancelled() => {5}
+                _ = tokio::time::sleep(std::time::Duration::from_secs(9999)) => {99}
+            }
+        });
+
+        let cancel_handle = tokio::spawn(async move {
+            tokio::time::sleep(std::time::Duration::from_millis(10)).await;
+            wrapper_token.cancel_with_post_action().await;
+        });
+
+        assert_eq!(5, join_handle.await.unwrap());
+        cancel_handle.await.unwrap();
     }
 }
 
