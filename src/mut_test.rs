e1ac4f4e31654f15664710aba33d956479824223
Styfjion <33060090+Styfjion@users.noreply.github.com>
2023-02-25 09:25:03 +0800

    Update mut_test.rs

--- src/mut_test.rs
+++ src/mut_test.rs
@@ -29,7 +29,7 @@ impl Data {
         if let Some(val) = self.check() {
             //self.inner.set_id(&val.id); // val引用持续存在，val为self函数的返回值引用，生命周期和self相同，因此此处和self.inner同时存在，冲突
             //self.inner.set_name_str(&val.name); // 同上
-            self.inner.set_name(val.name.clone()); // val使用完毕，编译器认为val的生命周期已经结束，此处编译通过
+            self.inner.set_name(val.name.clone()); // 基于声明周期NLL规则，val使用完毕，编译器认为val的生命周期已经结束，此处编译通过
             //self.inner.set_name(val.name.clone()); // 再次使用val，val的生命周期延续，因此不注释此行的话，上面一行报错
         }
     }
