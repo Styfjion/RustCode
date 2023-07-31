a00f11a50555b8437f0e2fd434a7695f2f33ec55
Styfjion <33060090+Styfjion@users.noreply.github.com>
2023-07-31 19:05:22 +0800

    Update main.rs

--- src/tokio_channel_practise/main.rs
+++ src/tokio_channel_practise/main.rs
@@ -5,31 +5,37 @@ mod task;
 use crate::client::ClientMessage;
 use crate::client_listener::Listener;
 use crate::task::Task;
+use chrono::Local;
 use tokio::select;
 use tokio_util::sync::CancellationToken;
 
 type StdError = Box<dyn std::error::Error + Send + Sync>;
 
-#[tokio::main]
-async fn main() -> Result<(), StdError> {
+async fn execute_job(task_secs: u64) -> Result<(), StdError> {
     // 准备通道
     let (tx, rx) = tokio::sync::mpsc::channel(32);
+    let mut listener = Listener::new(rx);
+
+    // 独立启动客户端，开始监听
+    let listener = tokio::spawn(async move { listener.listen().await });
     let tx_2 = tx.clone();
     let tx_3 = tx.clone();
 
-    // 独立启动客户端，开始监听
-    let listener = tokio::spawn(async move { Listener::listen(rx).await });
-
     // 接收启动消息
-    let start_msg = Task::receive_message(tx).await?;
+    let start_msg = Task::receive_message(tx, CancellationToken::new()).await?;
 
+    let start_time = Local::now();
     // 开始执行任务
     let cancel_token = CancellationToken::new();
     let cloned_token = cancel_token.clone();
-    let mut task = tokio::spawn(async move { Task::execute_task(start_msg, cloned_token).await });
+    let mut task =
+        tokio::spawn(async move { Task::execute_task(start_msg, cloned_token, task_secs).await });
 
     // 监听是否收到取消消息
-    let cancel_task = tokio::spawn(async move { Task::receive_message(tx_2).await });
+    let channel_token = CancellationToken::new();
+    let cloned_channel_token = channel_token.clone();
+    let cancel_task =
+        tokio::spawn(async move { Task::receive_message(tx_2, cloned_channel_token).await });
 
     select! {
         result = &mut task => {
@@ -39,7 +45,7 @@ async fn main() -> Result<(), StdError> {
                         Ok(val) => {
                             println!("job has been done, val is {:?}", val);
                             // 发送结果消息
-                            cancel_token.cancel();
+                            channel_token.cancel();
                             Task::send_result(tx_3, ClientMessage::Result {id: 1, status: "Success".to_string(), code: 0}).await?;
                         },
                         Err(error) => eprintln!("job task error is {:?}", error)
@@ -67,17 +73,38 @@ async fn main() -> Result<(), StdError> {
         }
     }
     listener.await?;
-    println!("program has been finished");
+    println!(
+        "program has been finished, time use is {}s",
+        (Local::now() - start_time).num_seconds()
+    );
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), StdError> {
+    execute_job(1).await?;
+    println!("------------------------------");
+    execute_job(10).await?;
     Ok(())
 }
 
 /*
-    task receive msg is Start { id: 1, name: "job_1", time: 2023-07-30T18:22:56.972797+08:00 }
-    start task, start message is Start { id: 1, name: "job_1", time: 2023-07-30T18:22:56.972797+08:00 }
+    task receive msg is Start { id: 1, name: "job_1", time: 2023-07-31T19:03:26.337721500+08:00 }
+    start task, start message is Start { id: 1, name: "job_1", time: 2023-07-31T19:03:26.337721500+08:00 }
+    task has been done
+    job has been done, val is "Success"
+    channel listener has been cancelled from job, listening is stopped
+    task receive msg is Cancel { id: 0 }
+    client 1 Send message success, message is Result { id: 1, status: "Success", code: 0 }
+    send task response code is 0
+    program has been finished, time use is 2s
+    ------------------------------
+    task receive msg is Start { id: 1, name: "job_1", time: 2023-07-31T19:03:28.357323600+08:00 }
+    start task, start message is Start { id: 1, name: "job_1", time: 2023-07-31T19:03:28.357323600+08:00 }
     task receive msg is Cancel { id: 1 }
     job has received cancel message is Cancel { id: 1 }, begin stop task
     task has been cancelled from job, task is stopped
     client 1 Send message success, message is Result { id: 1, status: "Cancelled", code: 1 }
     send task response code is 0
-    program has been finished
+    program has been finished, time use is 6s
 */
