f64d0ed5d83749451607481aee15b576adafe72d
Styfjion <33060090+Styfjion@users.noreply.github.com>
2023-07-31 19:05:40 +0800

    Update client.rs

--- src/tokio_channel_practise/client.rs
+++ src/tokio_channel_practise/client.rs
@@ -1,4 +1,6 @@
 use chrono::{DateTime, Local};
+use std::fs::read;
+use tokio_util::sync::CancellationToken;
 
 #[derive(Debug)]
 pub enum ClientMessage {
@@ -33,7 +35,17 @@ impl Client {
         0
     }
 
-    pub async fn receive(&mut self) -> ClientMessage {
+    pub async fn receive(&mut self, cancel_token: CancellationToken) -> ClientMessage {
+        tokio::select! {
+            result = self.receive_impl() => result,
+            _ = cancel_token.cancelled() => {
+                println!("channel listener has been cancelled from job, listening is stopped");
+                ClientMessage::Cancel{id: 0}
+            }
+        }
+    }
+
+    async fn receive_impl(&mut self) -> ClientMessage {
         if self.cnt == 0 {
             self.cnt += 1;
             ClientMessage::Start {
@@ -43,7 +55,7 @@ impl Client {
             }
         } else {
             //模拟监听取消
-            tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+            tokio::time::sleep(std::time::Duration::from_secs(5)).await;
             ClientMessage::Cancel { id: self.cnt }
         }
     }
