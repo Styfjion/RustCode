7c7c69ec73b329e1deeeb5c37ff72a3ddc5ca0c6
Styfjion <33060090+Styfjion@users.noreply.github.com>
2023-07-31 19:05:59 +0800

    Update client_listener.rs

--- src/tokio_channel_practise/client_listener.rs
+++ src/tokio_channel_practise/client_listener.rs
@@ -1,11 +1,13 @@
+use crate::client::{Client, ClientMessage};
 use tokio::sync::mpsc::Receiver;
 use tokio::sync::oneshot;
-use crate::client::{Client, ClientMessage};
+use tokio_util::sync::CancellationToken;
 
 #[derive(Debug)]
 pub enum Message {
     Receive {
         resp: oneshot::Sender<ClientMessage>,
+        cancel_token: CancellationToken,
     },
     Send {
         message: ClientMessage,
@@ -14,21 +16,28 @@ pub enum Message {
 }
 
 pub struct Listener {
-    
+    receive_channel: Receiver<Message>,
+    client: Client,
 }
 
 impl Listener {
-    pub async fn listen(mut receive_channel: Receiver<Message>) {
-        let mut client = Client::default();
-        while let Some(cmd) = receive_channel.recv().await {
+    pub fn new(receive_channel: Receiver<Message>) -> Self {
+        Listener {
+            receive_channel,
+            client: Client::default(),
+        }
+    }
+
+    pub async fn listen(&mut self) {
+        while let Some(cmd) = self.receive_channel.recv().await {
             match cmd {
-                Message::Receive { resp } => {
-                    let receive_msg = client.receive().await;
+                Message::Receive { resp, cancel_token } => {
+                    let receive_msg = self.client.receive(cancel_token).await;
                     // 忽略发送结果
                     let _ = resp.send(receive_msg);
                 }
                 Message::Send { message, resp } => {
-                    let send_result = client.send_msg(message).await;
+                    let send_result = self.client.send_msg(message).await;
                     let _ = resp.send(send_result);
                 }
             }
